317b7733a81fc3828716c631dc79c536aca70e63
Mifras <mifras@live.com>
2017-09-30 11:15:36 -0400

    Testing parallax effect

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
   $(".lander-text").fadeIn(1000);
+
+
+
+  (function(){
+
+    var parallax = document.querySelectorAll(".parallax"),
+        speed = 0.5;
+
+    window.onscroll = function(){
+      [].slice.call(parallax).forEach(function(el,i){
+
+        var windowYOffset = window.pageYOffset,
+            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+        el.style.backgroundPosition = elBackgrounPos;
+
+      });
+    };
+
+  })();
+
+
 })
