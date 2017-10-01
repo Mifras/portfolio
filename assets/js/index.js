d1c7f8e3ef9ae50a39743c1ab7bf46f7d075d5dd
Mifras <mifras@live.com>
2017-09-30 21:17:55 -0400

    Refactor code and add vowel-based fade in of 'n' in 'an' as necessary

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
   $(".lander-text").fadeIn(1000);
 
-  var carouselText = ["SOFTWARE", "HARDWARE", "PORTFOLIO", "BLOG"];
+  // Main text-carousel
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var carouselText = ["SOFTWARE", "HARDWARE", "ADVANCED", "PORTFOLIO", "EXPLORATIVE", "BLOG"];
   var counter = 0;
   var carouselElement = $('.text-carousel');
   setTimeout(carouselChangeText, 2000);
 
-////////////////////////////////////////////////////////////////////////////////
-//                                  Functions                                 //
-////////////////////////////////////////////////////////////////////////////////
-
   // Carousel Function
   function carouselChangeText() {
     $(carouselElement).slideUp(1000, function() {
+      vowels.includes(carouselText[counter].charAt(0).toLowerCase()) ? $('.sub-2-1').fadeIn() : $('.sub-2-1').fadeOut();
       this.innerHTML = carouselText[counter];
       counter = ++counter % carouselText.length;
       $(this).slideDown(1000, carouselChangeText)
